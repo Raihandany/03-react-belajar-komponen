879455cb5341f8866538fabd66994aa8db3364a6
Raihandany <raihandaniy@gmail.com>
2024-03-14 15:15:53 +0700

    connect git repositori

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,13 @@
+import Profile from "./profile";
+
+export default function SideBar() {
+    // return(
+    //     <Profile/>
+    //     <ul>
+    //         <li>Mengerjakan Pr</li>
+    //         <li>Mengerjakan Pr</li>
+    //         <li>Mengerjakan Pr</li>
+    //     </ul>
+    // );
+    
+}
